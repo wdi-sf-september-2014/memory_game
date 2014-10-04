5a04e46ddf3353de5735f13a2092f6a34f963c44
jangelroa <jangelroa@gmail.com>
2014-10-04 00:44:15 -0700

    Everything done but shuffle

--- jose_angel_roa_gomez/app.js
+++ jose_angel_roa_gomez/app.js
@@ -9,7 +9,8 @@ var cells = document.querySelectorAll("table div");
 
 for(var i =0 ; i < animals.length ; i++) {
 
-	cells[i].setAttribute("style", "display:none");
+	//cells[i].setAttribute("style", "display:none");
+	cells[i].setAttribute("class", "unmatched");
 	cells[i].innerHTML = animals[i];
 
 }
@@ -18,17 +19,54 @@ for(var i =0 ; i < animals.length ; i++) {
 // show animal when I click a cell
 
 var elements = document.querySelectorAll("td");
+var count = 1;
+var itemOne, itemTwo;
+var element1, element2;
+var timeoutID;
 
 for(var i =0 ; i < elements.length ; i++) {
 
 	elements[i].addEventListener("click", function(){
 		
+		// var element1, element2;
+		var divShowing = this.querySelector("div");
+
+		if(divShowing.className === "unmatched"){
+
+			divShowing.setAttribute("class", "matched");
+			console.log(divShowing.class);
+
+			if(count === 1){			// first click
+				itemOne = divShowing.innerHTML;
+				element1 = divShowing;
+				count++;
+			} else {					// second click
+				itemTwo = divShowing.innerHTML;
+				element2 = divShowing;
+				count = 1;
+
+				if(itemOne === itemTwo) {
+				} else {
+					setTimeout(function(){
+					element1.setAttribute("class", "unmatched");
+					element2.setAttribute("class", "unmatched");
+					},500);
+				}
+			}
+			
+		}
+
+	});
+}
+
+
+
 
 	// I am clicking in the td, so this = td (clicked)
 		//console.log(this);
 	// I look for the div inside the td
 		//console.log(this.querySelector("div"));
 
-		this.querySelector("div").setAttribute("style", "display:block");
-	});
-}
+		//this.querySelector("div").setAttribute("style", "display:block");
+	// I get the innerHTML of the just showed div
+		//console.log(this.querySelector("div").innerHTML);
