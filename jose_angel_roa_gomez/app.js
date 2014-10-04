9b19b934478edc88fa8ca4d238db26dbfe496acd
jangelroa <jangelroa@gmail.com>
2014-10-03 17:49:17 -0700

    // define the array of animals // populate the table // show animal when I click a cell

--- jose_angel_roa_gomez/app.js
+++ jose_angel_roa_gomez/app.js
@@ -1,1 +1,34 @@
-//Your code here
+// define the array of animals
+var animals = ["Elephant", "Monkey", "Lion",
+			   "Bird", "Dog", "Cat",
+			   "Elephant", "Monkey", "Lion",
+			   "Bird", "Dog", "Cat" ];
+
+// populate the table
+var cells = document.querySelectorAll("table div");
+
+for(var i =0 ; i < animals.length ; i++) {
+
+	cells[i].setAttribute("style", "display:none");
+	cells[i].innerHTML = animals[i];
+
+}
+
+
+// show animal when I click a cell
+
+var elements = document.querySelectorAll("td");
+
+for(var i =0 ; i < elements.length ; i++) {
+
+	elements[i].addEventListener("click", function(){
+		
+
+	// I am clicking in the td, so this = td (clicked)
+		//console.log(this);
+	// I look for the div inside the td
+		//console.log(this.querySelector("div"));
+
+		this.querySelector("div").setAttribute("style", "display:block");
+	});
+}
