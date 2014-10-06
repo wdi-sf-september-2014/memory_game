e53d4710c358ae8342871beac13d7830fa0163c5
Steve <gon138@gmail.com>
2014-10-05 22:25:41 -0700

    complete version 2

--- kung_chan/app.js
+++ kung_chan/app.js
@@ -1,23 +1,46 @@
-//1 Even I passed divs_array into the function, the array cannnot be changed within the function, 
-// I can only return it and assign to the global vairable again.
+//1 Even I passed divs_array into a function, the array cannnot be changed within the function, 
+//1 I can only return it and assign it to the global vairable again. This introduces a very funky bug.
+//1 That is if I change the signature of the previous function from 
+//1 (total_number_of_cards, divs_array) to (divs_array, total_number_of_cards)
+//1 the program will break.
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
 
 	var total_number_of_cards = 8;
 	var divs_array = [];
+	var divs_holder_array = [];
 
-	push_divs_to_divs_array										(total_number_of_cards, divs_array);
+//	Function Name												Signature
 
-	insert_image_tag_to_divs_in_divs_array						(total_number_of_cards, divs_array);
+	push_divs_to_divs_array										(divs_array, total_number_of_cards);
 
-	divs_array = clone_the_divs_array_and_add_clone_class		(total_number_of_cards, divs_array); //1
+	insert_image_tag_to_divs_in_divs_array						(total_number_of_cards, divs_array); // insert_image_tag_to_divs_in_divs_array						(divs_array, total_number_of_cards);
 
-	add_style_to_divs_array										(total_number_of_cards, divs_array);
-
-	add_click_listener_to_divs_in_divs_array					(divs_array);
+	divs_array = clone_the_divs_array_and_add_clone_class		(divs_array, total_number_of_cards); //1
 
 	fisher_yates_shuffle_the_divs_array							(divs_array);
 
-	add_divs_to_container										(divs_array);
+//  Version 2 -- with cover
+
+	push_divs_array_to_divs_holder_array						(divs_array, divs_holder_array);
+
+	add_card_cover_to_divs_holder_array							(divs_holder_array);
+
+	add_style_to_divs_holder_array								(divs_holder_array, total_number_of_cards);
+
+	add_click_listener_to_divs_in_divs_holder_array				(divs_holder_array);
+
+	add_divs_holder_to_container								(divs_holder_array);
+
+	add_button_to_toggle_cover									();
+
+
+//  Version 1 -- without cover
+
+	// add_style_to_divs_array										(divs_array, total_number_of_cards);
+
+	// add_click_listener_to_divs_in_divs_array					(divs_array);
+
+	// add_divs_to_container										(divs_array);
 
 })
--- kung_chan/functions.js
+++ kung_chan/functions.js
@@ -1,9 +1,34 @@
 //2 I cannot pass parameters without invoking the handler function, set up a gldbal variable instead.
 
 var clicked_card;
+var clicked_card_holder;
 
+function add_button_to_toggle_cover() {
+	$button = document.createElement('button');
+	$button.innerHTML = 'Toggle Cover';
+	$button.setAttribute('onclick', 'toggle_cover()')
+	document.getElementsByTagName('body')[0].appendChild($button);
+}
 
-function push_divs_to_divs_array(total_number_of_cards, divs_array) {
+function toggle_cover() {
+	[].slice.call(document.querySelectorAll('.cover')).forEach(function($cover){
+		if ($cover.classList.contains('remove_cover')) {
+			$cover.classList.remove('remove_cover');
+		} else {
+			$cover.classList.add('remove_cover');
+		}
+	})
+}
+
+function push_divs_array_to_divs_holder_array(divs_array, divs_holder_array) {
+	for (var i = 0; i < divs_array.length; i++) {
+		var $div_holder = document.createElement('div');
+		$div_holder.appendChild(divs_array[i])
+		divs_holder_array.push($div_holder);
+	}	
+}
+
+function push_divs_to_divs_array(divs_array, total_number_of_cards) {
 	for (var i = 1; i <= total_number_of_cards; i++) {
 		var $div = document.createElement('div');
 		$div.setAttribute('class', 'card' + i);
@@ -11,17 +36,32 @@ function push_divs_to_divs_array(total_number_of_cards, divs_array) {
 	}
 }
 
-function insert_image_tag_to_divs_in_divs_array(total_number_of_cards, divs_array) {
-
-	for (var i = 1; i <= total_number_of_cards; i++) {
-		var $imgTag = document.createElement('img');
-		$imgTag.setAttribute('width','100');			
-		$imgTag.setAttribute('src','http://api.mtgdb.info/content/card_images/15' + i + '.jpeg');
-		divs_array[i-1].appendChild($imgTag);
+function add_card_cover_to_divs_holder_array(divs_holder_array) {
+	for (var i = 0; i < divs_holder_array.length; i++) {
+		var $div = document.createElement('div');
+		var $img = document.createElement('img');
+		$img.setAttribute('width','100');	
+		$img.setAttribute('src','http://api.mtgdb.info/content/card_images/150.jpeg');
+		$div.classList.add('cover');	
+		$div.setAttribute('width','100');
+		$div.appendChild($img);
+		divs_holder_array[i].appendChild($div);
 	}
 }
 
-function clone_the_divs_array_and_add_clone_class(total_number_of_cards, divs_array) {
+function insert_image_tag_to_divs_in_divs_array(total_number_of_cards, divs_array) {
+// function insert_image_tag_to_divs_in_divs_array(divs_array, total_number_of_cards) {
+	// console.log(total_number_of_cards, divs_array)
+
+	for (var i = 1; i <= total_number_of_cards; i++) {
+		var $img = document.createElement('img');
+		$img.setAttribute('width','100');			
+		$img.setAttribute('src','http://api.mtgdb.info/content/card_images/15' + i + '.jpeg');
+		divs_array[i-1].appendChild($img);
+	}
+}
+
+function clone_the_divs_array_and_add_clone_class(divs_array, total_number_of_cards) {
 	var $clone_divs_array = [];
 	for (var i = 0; i < divs_array.length; i++) {
 		var $newNode = divs_array[i].cloneNode();
@@ -35,9 +75,15 @@ function clone_the_divs_array_and_add_clone_class(total_number_of_cards, divs_ar
 	return divs_array.concat($clone_divs_array)  //1
 }
 
-function add_style_to_divs_array(total_number_of_cards, divs_array) {
+function add_style_to_divs_array(divs_array, total_number_of_cards) {
 	for (var i = 0; i < divs_array.length; i++) {
 		divs_array[i].classList.add('card_holder')
+	}
+}
+
+function add_style_to_divs_holder_array(divs_holder_array, total_number_of_cards) {
+	for (var i = 0; i < divs_holder_array.length; i++) {
+		divs_holder_array[i].classList.add('card_holder')
 	}
 }
 
@@ -45,6 +91,13 @@ function add_divs_to_container(divs_array) {
 	$container = document.querySelector('.container');
 	for (var i = 0; i < divs_array.length; i++) {
 		$container.appendChild(divs_array[i]);
+	}
+}
+
+function add_divs_holder_to_container(divs_holder_array) {
+	$container = document.querySelector('.container');
+	for (var i = 0; i < divs_holder_array.length; i++) {
+		$container.appendChild(divs_holder_array[i]);
 	}
 }
 
@@ -65,6 +118,37 @@ return array;
 function add_click_listener_to_divs_in_divs_array(divs_array) {
 	for (var i = 0; i < divs_array.length; i++) {
 		divs_array[i].addEventListener('click', click_handler)   //2
+	}
+}
+
+function add_click_listener_to_divs_in_divs_holder_array(divs_holder_array) {
+	for (var i = 0; i < divs_holder_array.length; i++) {
+		divs_holder_array[i].addEventListener('click', holder_click_handler)  
+	}	
+}
+
+function holder_click_handler(){
+	if (clicked_card_holder === undefined) {
+		// first click
+		clicked_card_holder = this;
+		clicked_card_holder.removeEventListener('click', holder_click_handler);
+		clicked_card_holder.children[1].classList.add('clicked')
+	} else {
+		// second click
+		if (this.childNodes[0].classList[0] === clicked_card_holder.childNodes[0].classList[0]) {
+			// matched
+			console.log('matched')
+			this.removeEventListener('click', holder_click_handler);
+			clicked_card_holder.children[1].classList.remove('clicked');
+			clicked_card_holder.children[1].classList.add('matched');
+			this.children[1].classList.add('matched');
+		} else {
+			// not matched
+			console.log('not matched')
+			clicked_card_holder.addEventListener('click', holder_click_handler)
+			clicked_card_holder.children[1].classList.remove('clicked')
+		}
+		clicked_card_holder = undefined;
 	}
 }
 
@@ -92,3 +176,4 @@ function click_handler(){
 		clicked_card = undefined;
 	}
 }		
+
